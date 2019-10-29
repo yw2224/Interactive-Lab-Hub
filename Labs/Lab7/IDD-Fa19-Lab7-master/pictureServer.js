afae849808d6de372ae8a111e9de3108f3ad68ff
yw2224 <yw2224@cornell.edu>
2019-10-29 19:22:56 -0400

    first commit

---
+++ Labs/Lab7/IDD-Fa19-Lab7-master/pictureServer.js
@@ -0,0 +1,158 @@
+/*
+server.js
+
+Authors:David Goedicke (da.goedicke@gmail.com) & Nikolas Martelaro (nmartelaro@gmail.com)
+
+This code is heavily based on Nikolas Martelaroes interaction-engine code (hence his authorship).
+The  original purpose was:
+This is the server that runs the web application and the serial
+communication with the micro controller. Messaging to the micro controller is done
+using serial. Messaging to the webapp is done using WebSocket.
+
+//-- Additions:
+This was extended by adding webcam functionality that takes images remotely.
+
+Usage: node server.js SERIAL_PORT (Ex: node server.js /dev/ttyUSB0)
+
+Notes: You will need to specify what port you would like the webapp to be
+served from. You will also need to include the serial port address as a command
+line input.
+*/
+
+var express = require('express'); // web server application
+var app = express(); // webapp
+var http = require('http').Server(app); // connects http library to server
+var io = require('socket.io')(http); // connect websocket library to server
+var serverPort = 8000;
+var SerialPort = require('serialport'); // serial library
+var Readline = SerialPort.parsers.Readline; // read serial data as lines
+//-- Addition:
+var NodeWebcam = require( "node-webcam" );// load the webcam module
+
+//Google Vision
+const vision = require('@google-cloud/vision');
+
+// Creates a client
+const client = new vision.ImageAnnotatorClient();
+
+//---------------------- WEBAPP SERVER SETUP ---------------------------------//
+// use express to create the simple webapp
+app.use(express.static('public')); // find pages in public directory
+
+// check to make sure that the user provides the serial port for the Arduino
+// when running the server
+if (!process.argv[2]) {
+  console.error('Usage: node ' + process.argv[1] + ' SERIAL_PORT');
+  process.exit(1);
+}
+
+// start the server and say what port it is on
+http.listen(serverPort, function() {
+  console.log('listening on *:%s', serverPort);
+});
+//----------------------------------------------------------------------------//
+
+//--Additions:
+//----------------------------WEBCAM SETUP------------------------------------//
+//Default options
+var opts = { //These Options define how the webcam is operated.
+    //Picture related
+    width: 1280, //size
+    height: 720,
+    quality: 100,
+    //Delay to take shot
+    delay: 0,
+    //Save shots in memory
+    saveShots: true,
+    // [jpeg, png] support varies
+    // Webcam.OutputTypes
+    output: "jpeg",
+    //Which camera to use
+    //Use Webcam.list() for results
+    //false for default device
+    device: false,
+    // [location, buffer, base64]
+    // Webcam.CallbackReturnTypes
+    callbackReturn: "location",
+    //Logging
+    verbose: false
+};
+var Webcam = NodeWebcam.create( opts ); //starting up the webcam
+//----------------------------------------------------------------------------//
+
+
+
+//---------------------- SERIAL COMMUNICATION (Arduino) ----------------------//
+// start the serial port connection and read on newlines
+const serial = new SerialPort(process.argv[2], {});
+const parser = new Readline({
+  delimiter: '\r\n'
+});
+
+// Read data that is available on the serial port and send it to the websocket
+serial.pipe(parser);
+parser.on('data', function(data) {
+  console.log('Data:', data);
+  io.emit('server-msg', data);
+  if (data === 'light') {
+      var imageName = new Date().toString().replace(/[&\/\\#,+()$~%.'":*?<>{}\s-]/g, '');
+      NodeWebcam.capture('public/'+imageName, opts, function( err, data ) {
+          io.emit('newPicture',(imageName+'.jpg'));
+	  detectFaces('/home/pi/IDD-Fa19-Lab7/public/'+imageName+'.jpg');
+      });
+  }
+});
+//----------------------------------------------------------------------------//
+
+
+//---------------------- WEBSOCKET COMMUNICATION (web browser)----------------//
+// this is the websocket event handler and say if someone connects
+// as long as someone is connected, listen for messages
+io.on('connect', function(socket) {
+  console.log('a user connected');
+
+  // if you get the 'ledON' msg, send an 'H' to the Arduino
+  socket.on('ledON', function() {
+    console.log('ledON');
+    serial.write('H');
+  });
+
+  // if you get the 'ledOFF' msg, send an 'L' to the Arduino
+  socket.on('ledOFF', function() {
+    console.log('ledOFF');
+    serial.write('L');
+  });
+
+  //-- Addition: This function is called when the client clicks on the `Take a picture` button.
+  socket.on('takePicture', function() {
+    /// First, we create a name for the new picture.
+    /// The .replace() function removes all special characters from the date.
+    /// This way we can use it as the filename.
+    var imageName = new Date().toString().replace(/[&\/\\#,+()$~%.'":*?<>{}\s-]/g, '');
+
+    console.log('making a making a picture at'+ imageName); // Second, the name is logged to the console.
+
+    //Third, the picture is  taken and saved to the `public/`` folder
+    NodeWebcam.capture('public/'+imageName, opts, function( err, data ) {
+    io.emit('newPicture',(imageName+'.jpg')); ///Lastly, the new name is send to the client web browser.
+    /// The browser will take this new name and load the picture from the public folder.
+  });
+
+  });
+  // if you get the 'disconnect' message, say the user disconnected
+  socket.on('disconnect', function() {
+    console.log('user disconnected');
+  });
+});
+//----------------------------------------------------------------------------//
+
+async function detectFaces(inputFile) {
+  // Make a call to the Vision API to detect the faces
+  const request = {image: {source: {filename: inputFile}}};
+  const results = await client.faceDetection(request);
+  const faces = results[0].faceAnnotations;
+  const numFaces = faces.length;
+  console.log('Found ' + numFaces + (numFaces === 1 ? ' face' : ' faces'));
+  io.emit('faces',(numFaces));
+  return faces;
+}
---
+++ Labs/Lab7/IDD-Fa19-Lab7-master/public/client.js
@@ -0,0 +1,62 @@
+/*
+client.js
+
+Author: Nikolas Martelaro (nmartelaro@gmail.com)
+Extended: David Goeicke (da.goedicke@gmail.com)
+Purpose: This run the interactivity and communication for the web app. This file
+is served to the users web browser and executes on the browser.
+
+Usage: This file is called automatically when the webpage is served.
+
+//--Addition. Added a button handling for the `Take a picture` button.
+*/
+
+// WebSocket connection setup
+var socket = io();
+
+// send out LedOn message over socket
+function ledON() {
+  socket.emit('ledON');
+}
+
+// send out ledOFF message over socket
+function ledOFF() {
+  socket.emit('ledOFF');
+}
+
+//-- Addition: Forward the `Take a picture` button-press to the webserver.
+function takePicture(){
+  socket.emit('takePicture');
+}
+
+//-- Addition: This function receives the new image name and applies it to html element.
+socket.on('faces', function(msg) {
+    if (msg === 0) {
+        document.getElementById("message").innerHTML = "Oops, I can't see you!";
+    } else {
+	document.getElementById("message").innerHTML = "Welcome!";
+    }
+});
+
+socket.on('newPicture', function(msg) {
+  document.getElementById('pictureContainer').src=msg;
+});
+// read the data from the message that the server sent and change the
+// background of the webpage based on the data in the message
+socket.on('server-msg', function(msg) {
+  msg = msg.toString();
+  console.log('msg:', msg);
+  switch (msg) {
+    case "light":
+      document.body.style.backgroundColor = "white";
+      console.log("white")
+      break;
+    case "dark":
+      document.body.style.backgroundColor = "black";
+      console.log("black");
+      break;
+    default:
+      //console.log("something else");
+      break;
+  }
+});
